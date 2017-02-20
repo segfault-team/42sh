f99e98dd9e6864058c1405408bf04945cd2756bc
kboddez <kboddez@student.42.fr>
2017-02-20 13:01:35 +0100

    ctrl + arrow is now ok + creation of new file with ctrl + home/end

--- srcs/tcaps/tcaps_ctrl.c
+++ srcs/tcaps/tcaps_ctrl.c
@@ -6,7 +6,7 @@
 /*   By: kboddez <kboddez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/01 13:08:16 by kboddez           #+#    #+#             */
-/*   Updated: 2017/02/16 15:45:46 by kboddez          ###   ########.fr       */
+/*   Updated: 2017/02/20 12:57:54 by kboddez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,79 +23,36 @@ static int	ctrl_up_is_not_on_prompt(t_env *e)
 **  INSTRUCTION FOR "Ctrl + l" KEYS
 **
 **  cl: clear active window
+**  To do : reset positions after clear && check buffer after ctrl+l
 */
 
 void	tcaps_clear(t_env *e)
 {
 	xputs("cl");
-	strfree(&e->line);
-	clear_cmd(e);
+	ft_putstr(e->prompt);
 }
 
 /*
-**  INSTRUCTION FOR "Ctrl + a" KEYS || cmd: home
-**
-**	cr: return at the begining of the line
-**	nd: move cursor once on the right
+**  INSTRUCTION FOR "Ctrl + ARROW ->" KEYS
 */
-
-void	tcaps_ctrl_home(t_env *e)
-{
-	int		i;
-	int	l;
-
-	l = TCAPS.nb_read;
-	while (l--)
-	{
-		xputs("le");
-		TCAPS.nb_move = 0;
-	}
-	xputs("cr");
-	i = ft_strlen(e->prompt);
-	while (i--)
-		xputs("nd");
-	tcaps_recalc_pos(e);
-}
-
-/*
-**  INSTRUCTION FOR "Ctrl + e" KEYS || cmd: end
-**
-**	cr: return at the begining of the line
-**	nd: move cursor once on the right
-*/
-
-void	tcaps_ctrl_end(t_env *e)
-{
-	tcaps_recalc_pos(e);
-	while (TCAPS.nb_move < TCAPS.nb_read)
-		move_right(e);
-}
-
-/*
- **  INSTRUCTION FOR "Ctrl + ARROW ->" KEYS
- */
 
 void	tcaps_ctrl_mov_right(t_env *e)
 {
-	int		i;
+	int	i;
 
 	i = TCAPS.nb_move;
-	while ((i > 0 && i != TCAPS.nb_read && e->line[i - 1] == ' ')
-			|| (!i && TCAPS.nb_read))
+	while (i < TCAPS.nb_read && ft_isalpha(e->line[i + 1]))
 	{
 		move_right(e);
 		++i;
 	}
-	while (i > 0 && i != TCAPS.nb_read && e->line[i - 1] != ' ')
+	while (i < TCAPS.nb_read && !ft_isalpha(e->line[i + 1]))
 	{
 		move_right(e);
 		++i;
 	}
-	while (e->line[i] == '-' || e->line[i] == ' ')
-	{
+	if (i + 1 < TCAPS.nb_read)
 		move_right(e);
-		++i;
-	}
 }
 
 /*
@@ -107,22 +64,17 @@ void	tcaps_ctrl_mov_left(t_env *e)
 	int	i;
 
 	i = TCAPS.nb_move;
-	while (i > 0 && (e->line[i - 1] == ' ' || e->line[i - 1] == '-'))
+	while (i > 0 && !ft_isalpha(e->line[i - 1]))
 	{
 		xputs("le");
 		--TCAPS.nb_move;
 		--i;
 	}
-	while ((i && e->line[i - 1] != ' '))
+	while (i >= 0 && ft_isalpha(e->line[i - 1]))
 	{
 		xputs("le");
 		--TCAPS.nb_move;
 		--i;
-	}
-	while (e->line[i] == '-')
-	{
-		move_right(e);
-		++i;
 	}
 }
 
