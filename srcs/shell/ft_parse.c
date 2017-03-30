fe7e752bb423f1132c55b9c84c820821bbbdb147
vlistrat <vlistrat@student.42.fr>
2017-03-30 14:52:04 +0200

    fix aggregators + single cmd

--- srcs/shell/ft_parse.c
+++ srcs/shell/ft_parse.c
@@ -120,10 +120,13 @@ int				ft_iter_cmds(t_env *e, char *cmds_i)
 				struct_find_red(e);
 			ret = ft_exec_cmd(e, e->cat[i]);
 		}
-		else if (!is_last_cmd(e, RED_INDEX))
+		else// if (!is_last_cmd(e, RED_INDEX))
 			ret = redir_exec_open(i, e);
-		else
-			ret = ft_exec_cmd(e, e->cat[i]);
+//		else
+//		{
+//			struct_find_red(e);
+//			ret = ft_exec_cmd(e, e->cat[i]);
+//		}
 		reset_last_ret(e, ret);
 		if (is_output_redir(e, RED_INDEX))
 			redir_fill_output(e);
