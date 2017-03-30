7e1ac115d128345470cfade385b807903d6f6ef7
Ayoub LEGHMARI <aleghmar@e1r11p3.42.fr>
2017-03-30 17:58:15 +0200

    remove useless file and comments

--- srcs/parse_command/check_parsing.c
+++ srcs/parse_command/check_parsing.c
@@ -8,9 +8,6 @@ void	check_parsing_double(t_env *e, int *i, char c)
 	if (*i > 1 && e->line[*i - 1] && e->line[*i - 2]
 			 && e->line[*i - 1] == c && e->line[*i - 2] == c)
 		insert_char(e, ' ', (*i)++);
-//if (*i + 1 < (int)ft_strlen(e->line) &&
-//		e->line[*i + 1] && e->line[*i + 1] != c && e->line[*i + 1] != ' ')
-//		insert_char(e, ' ', ++(*i));
 }
 
 void	check_parsing_simple(t_env *e, int *i, char c)
