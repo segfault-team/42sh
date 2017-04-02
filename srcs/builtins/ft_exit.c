1ab7a59d3abd8e917fa13eb6cb6c7f83190d80f7
lfabbro <lfabbro@student.42.fr>
2017-04-02 02:11:33 +0200

    fix exit

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -2,15 +2,15 @@
 
 void		ft_exit(t_env *e, char **cmd)
 {
-	if (e->cmd && e->cmd[1])
+	if (cmd && cmd[1])
 	{
-		if (!ft_strisdigit(e->cmd[1]))
+		if (!ft_strisdigit(cmd[1]))
 		{
 			e->exit = 255;
-			ft_error("exit", e->cmd[1], "numeric argument required");
+			ft_error("exit", cmd[1], "numeric argument required");
 		}
 		else
-			e->exit = ft_atoi(e->cmd[1]);
+			e->exit = ft_atoi(cmd[1]);
 	}
 	else
 		e->exit = 0;
