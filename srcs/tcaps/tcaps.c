740c78da3fc8fc48ff337b29943d8a56c5d2a2be
Ayoub LEGHMARI <aleghmar@e1r10p4.42.fr>
2017-04-06 18:03:37 +0200

    pas vraiment sur ... remove de tcaos_paste et set ws a -1 si pas trouve

--- srcs/tcaps/tcaps.c
+++ srcs/tcaps/tcaps.c
@@ -8,7 +8,7 @@ static void	tcaps_bis(t_env *e)
 		tcaps_del_fwd(e);
 	else if (tcaps_check_key(BUF, 27, 91, 49))
 		tcaps_ctrl_arrow(e);
-	else if (tcaps_check_key(BUF, 9, 0, 0) && !e->raw)
+	else if (tcaps_check_key(BUF, 9, 0, 0))
 		auto_completion(e);
 }
 
@@ -16,8 +16,8 @@ int			tcaps(t_env *e)
 {
 	if (BUF[0] == CTRL_D)
 		tcaps_ctrl_d(e);
-	else if (is_paste(BUF))
-		tcaps_paste(e, BUF);
+	/*else if (is_paste(BUF))
+		tcaps_paste(e, BUF);*/
 	else if (tcaps_is_printable(BUF) && NB_MOVE == NB_READ && !e->raw)
 		tcaps_insert(e);
 	else if (tcaps_check_key(BUF, 12, 0, 0))
