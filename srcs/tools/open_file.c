fa11b19f2bd929de0013e8015591217e9da2c35f
kboddez <kboddez@student.42.fr>
2017-04-12 11:33:21 +0200

    fix bug when redirect from directory

--- srcs/tools/open_file.c
+++ srcs/tools/open_file.c
@@ -2,12 +2,17 @@
 
 int	open_file(char *file, int flags, mode_t mode)
 {
-	int	fd;
+	struct stat	stat;
+	int			fd;
 
+	ft_bzero(&stat, sizeof(struct stat));
 	if (access(file, F_OK) < 0)
 		return (ft_error("no such file: ", file, NULL));
 	if (access(file, R_OK) < 0)
 		return (ft_error("permission denied: ", file, NULL));
+    lstat(file, &stat);
+	if (S_ISDIR(stat.st_mode))
+		return (ft_error("read failed", file, "is a directory"));
 	if ((fd = open(file, flags, mode)) == -1)
 		return (ft_error(file, "Resource temporarily unavailable", NULL));
 	return (fd);
