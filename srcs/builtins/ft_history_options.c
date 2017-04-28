d74506c3e5c19c753c2fb5835ea8ffff3ce592a6
Ayoub LEGHMARI <aleghmar@e1r10p4.42.fr>
2017-04-28 11:47:54 +0200

    Merge chelou ?

--- srcs/builtins/ft_history_options.c
+++ srcs/builtins/ft_history_options.c
@@ -2,7 +2,7 @@
 
 int		print_history(t_env *e, char **cmd)
 {
-	size_t	i;
+	int		i;
 	int		arg;
 	int		len_tab;
 
@@ -11,15 +11,16 @@ int		print_history(t_env *e, char **cmd)
 	len_tab = ft_tablen(e->history);
 	if (cmd[1])
 	{
-		arg = (size_t)atoi_bis(cmd[1]);
-		if (arg == -1)
-			return (history_error_with_id(cmd, SH_NAME, 2));
+		if ((arg = (size_t)atoi_bis(cmd[1])) == -1)
+			return (ft_error("history", cmd[1], "numeric argument required"));
 		i = ft_tablen(e->history) - arg - 1;
 	}
-	if (i + 1 > (size_t)len_tab)
+	if (i < 0)
 		i = -1;
+	else if (!is_only_numbers(cmd[1]) || i < 0)
+		return (ft_error("history", cmd[1], "numeric argument required"));
 	while (e->history[++i])
-		ft_printf("%d: %s\n", (i + 1), e->history[i]);
+		ft_printf("%d: %s\n", i, e->history[i]);
 	return (1);
 }
 
@@ -43,19 +44,21 @@ int		append_history_file_in_list(t_env *e)
 	int		i;
 
 	if ((history_fd = open(HIST_FILE, O_RDWR | O_CREAT, OFLAGS)) == -1)
-		return (ft_error(SH_NAME, "Cannot read", HIST_FILE));
+		return (ft_error("Cannot read", HIST_FILE, NULL));
 	len = ft_tablen(e->history) + 4096;
 	nb_lines = 0;
 	i = -1;
 	if ((new = malloc(sizeof(e->history) * len)) == NULL)
-		return (ft_error(SH_NAME, "Malloc failed.", NULL));
+		return (ft_error(NULL, "Malloc failed.", NULL));
 	while (e->history[++i])
-		new[i] = e->history[i];
+		new[i] = ft_strdup(e->history[i]);
 	new[i] = NULL;
 	while (++nb_lines < 4096 && get_next_line(history_fd, &new[i]) > 0)
 		++i;
 	new[++i] = NULL;
 	ft_close(history_fd);
+	if (e->history)
+		ft_free_tab(e->history);
 	e->history = new;
 	return (1);
 }
